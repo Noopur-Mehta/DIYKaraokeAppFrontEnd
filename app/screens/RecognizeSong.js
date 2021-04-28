b337d2b08c934d431ac0c78977a8abc32ffc5f9a
Noopur-Mehta <noopur.mehta@sjsu.edu>
2021-04-28 16:57:08 -0700

    Recording done

--- app/screens/RecognizeSong.js
+++ app/screens/RecognizeSong.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, View, Image,TouchableOpacity,Text} from 'react-native';
+import {SafeAreaView, StyleSheet, View, Image,TouchableOpacity,Text,Button } from 'react-native';
+import { Audio } from 'expo-av';
 
 function RecognizeSong(props) {
+    const [recording, setRecording] = React.useState();
+
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      }); 
+      console.log('Starting recording..');
+      const recording = new Audio.Recording();
+      await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+      await recording.startAsync(); 
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI(); 
+    console.log('Recording stopped and stored at', uri);
+  }
+
     return (
         <SafeAreaView style={styles.container}>
             <Image style={styles.logo} source={require("../assets/karaoke.png")} />
+            <Button
+        title={recording ? 'Stop Recording' : 'Start Recording'}
+        onPress={recording ? stopRecording : startRecording}
+      />
             </SafeAreaView>
     );
 }
